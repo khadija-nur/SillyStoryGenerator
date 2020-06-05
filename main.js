b6402e259bd2df10cbf5c53b2d712bddfd5d3aba
khadija <59174800+khadija-nur@users.noreply.github.com>
2020-06-05 15:15:29 +0100

    Bug-fix: customname is displayed when user inputs it

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 const story = document.querySelector("p");
-const customName = document.getElementById("customName").value;
-const generateBtn = document.querySelector("button");
 
+// console.log(customName)
+const generateBtn = document.querySelector("button");
 const content = {
     name: ["Scrappy-Doo", "Velma", "Daphne", "Shaggy"],
     country : ["Tuvalu", "Kribati", "Niue", "Vanuatu"],
-    price : "5"
+    price : 5
 };
 
 generateBtn.addEventListener ('click', result = () => {
+    let customName = document.querySelector("#customName").value;
+
     let randomPrice = price = "£" + content.price;
-    let name ;
-    console.log(customName)
+    let name = "Chicken Noodles";
+
     let randomNumberOfThree = Math.floor(Math.random()*3); 
     let randomName = content.name[randomNumberOfThree];
     let randomCountry = content.country[randomNumberOfThree];
+
     let checkedCountry = document.querySelector('input[name = country]:checked').value;
 
+    if (customName !== ''){
+
+        name = customName;
+    }
+    if (document.getElementById("US").checked){
+        randomPrice = "$" + (content.price)*1.26
+    }
     const storyText = `${name} once flew to ${randomCountry} from ${checkedCountry} with Scooby-Doo and the rest of the crew.
     ${randomName} bought souvenirs worth ${randomPrice} for the ghostbusters from Mars and aliens from Earth.`
-
-    if (customName.value !== ''){
-        name = customName.value;
-        console.log(name)
-    }
-    name = "Chicken Noodles";
-    console.log(name)
-    if (document.getElementById("US").checked){
-        randomPrice = "$" + content.price
-    }
     story.textContent = storyText;
 
 });
